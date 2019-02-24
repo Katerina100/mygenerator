99151a231e9bb2d7e59fac958e620f74f77d1181
Ekaterina Abramova <abramowakate@ya.ru>
2019-02-25 01:12:32 +0300

    Create xlsx file

--- src/main/java/com/mygenerator/app/App.java
+++ src/main/java/com/mygenerator/app/App.java
@@ -1,11 +1,21 @@
 package com.mygenerator.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            FileOutputStream file = new FileOutputStream(new File("people.xlsx"));
+            file.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("exception occured is " + e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("exception occured is " + e);
+            e.printStackTrace();
+        }
     }
 }
