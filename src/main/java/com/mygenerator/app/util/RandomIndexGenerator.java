57af793e8f13564a060abcc9c99b9b2bafd71a52
Ekaterina Abramova <abramowakate@ya.ru>
2019-03-03 19:11:41 +0300

    index add

--- src/main/java/com/mygenerator/app/util/RandomIndexGenerator.java
+++ src/main/java/com/mygenerator/app/util/RandomIndexGenerator.java
@@ -4,8 +4,12 @@ import java.util.Random;
 
 public class RandomIndexGenerator {
     public static String getNew() {
+        int min = 100000;
+        int max = 200000;
+        int diff = max - min;
         Random rand = new Random();
-        int number = rand.nextInt(999999);
+        int number = rand.nextInt(diff + 1);
+        number += min;
         return String.format("%06d", number);
     }
 }
