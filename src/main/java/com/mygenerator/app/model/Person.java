1a1836a94e2262d4a2096196e1a018923d141557
Ekaterina Abramova <abramowakate@ya.ru>
2019-03-03 18:58:36 +0300

    noset add

--- src/main/java/com/mygenerator/app/model/Person.java
+++ src/main/java/com/mygenerator/app/model/Person.java
@@ -37,96 +37,48 @@ public class Person {
         return this.lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getFirstName() {
         return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getPatronymic() {
         return this.patronymic;
     }
 
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
     public Date getBirthDate() {
         return this.birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
     }
 
     public String getInn() {
         return this.inn;
     }
 
-    public void setInn(String inn) {
-        this.inn = inn;
-    }
-
     public String getCountry() {
         return this.country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
     }
 
     public String getRegion() {
         return this.region;
     }
 
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
     public String getCity() {
         return this.city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getStreet() {
         return this.street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
     public int getHouse() {
         return this.house;
-    }
-
-    public void setHouse(int house) {
-        this.house = house;
     }
 
     public int getFlat() {
         return this.flat;
     }
 
-    public void setFlat(int flat) {
-        this.flat = flat;
-    }
-
     public String getIndex() {
         return this.index;
-    }
-
-    public void setIndex(String index) {
-        this.index = index;
     }
 
     @Override
@@ -134,3 +86,4 @@ public class Person {
         return getLastName() + " " + getFirstName() + " " + getPatronymic();
     }
 }
+
